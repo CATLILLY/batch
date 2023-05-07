05871d2ec33b1a9d5a704074602751712869eb76
®️Bylickilabs™️ <131188837+CATLILLY@users.noreply.github.com>
2023-05-07 19:29:12 +0200

    update #3
    update
    
    Co-Authored-By: CATLILLY <131188837+catlilly@users.noreply.github.com>
    Co-Authored-By: ®️BYLICKILABS™️ <109308073+bylickilabs@users.noreply.github.com>

--- Test.bat
+++ Test.bat
@@ -3,3 +3,5 @@ color C
 title @CATLILLY @bylickilabs
 
 :Hauptmenu
+title BYLICKILABS
+echo.echo ------------------------------------------------------------------------------------------------------------------------
